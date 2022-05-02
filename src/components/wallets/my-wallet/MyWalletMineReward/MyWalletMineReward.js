8ec513d5bdce4ae91aa9641f70ea1d6ebb10afaf
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2022-05-02 16:17:15 +0700

    add "Mine new block" button in MyWalletMineReward.js

--- src/components/wallets/my-wallet/MyWalletMineReward/MyWalletMineReward.js
+++ src/components/wallets/my-wallet/MyWalletMineReward/MyWalletMineReward.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+// MUI COMPONENTS
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
 const MyWalletMineReward = () => {
-  return <div>MyWalletMineReward</div>;
+  return (
+    <Container maxWidth="xs">
+      <Card raised sx={{ p: 2, mt: "25vh" }}>
+        <Grid container direction="column" spacing={1}>
+          <Grid item>
+            <Typography variant="h6">Mining Reward : 100</Typography>
+          </Grid>
+          <Grid item alignSelf="center">
+            <Button
+              variant="contained"
+              sx={{ textTransform: "none", textAlign: "center" }}
+            >
+              Mine new block
+            </Button>
+          </Grid>
+        </Grid>
+      </Card>
+    </Container>
+  );
 };
 
 export default MyWalletMineReward;
