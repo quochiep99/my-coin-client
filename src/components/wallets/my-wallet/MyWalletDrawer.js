1f09726871c8c02e03ec65b28c08e5bd31f9019e
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2022-05-02 14:58:36 +0700

    rename drawer items

--- src/components/wallets/my-wallet/MyWalletDrawer.js
+++ src/components/wallets/my-wallet/MyWalletDrawer.js
@@ -12,8 +12,11 @@ import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+
+// MUI ICONS
+import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
+import IosShareOutlinedIcon from "@mui/icons-material/IosShareOutlined";
 
 const drawerWidth = 240;
 
@@ -27,7 +30,7 @@ const MyWalletDrawer = () => {
       >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            Permanent drawer
+            Coinbase dashboard
           </Typography>
         </Toolbar>
       </AppBar>
@@ -46,25 +49,24 @@ const MyWalletDrawer = () => {
         <Toolbar />
         <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <ListItem button>
+            <ListItemIcon>
+              <PaidOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Balance" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <EmojiEventsOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Mine Reward" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <IosShareOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Send coin" />
+          </ListItem>
         </List>
       </Drawer>
       <Box
