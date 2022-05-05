93e65b77f0a2545d53cc4901e84a5ad8b5b7f2e7
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2022-05-05 11:55:06 +0700

    switch to useReducer instead of useState

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,39 +1,57 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 // MUI COMPONENTS
 import Container from "@mui/material/Container";
+
+import produce from "immer";
 
 // MY COMPONENTS
 import HomeCoinbaseWalletCard from "../../components/home/HomeCoinbaseWalletCard";
 import HomeCreateNewWalletStepper from "../../components/home/HomeCreateNewWalletStepper";
 import HomeImportWalletStepper from "../../components/home/HomeImportWalletStepper";
 
+const initialStates = {
+  homeCoinbaseWalletCard: true,
+  homeCreateNewWalletStepper: false,
+  homeImportWalletStepper: false,
+};
+
+const handlers = {
+  SHOW_CREATE_NEW_WALLET_STEPPER: (state, action) => {
+    return produce(state, (draft) => {
+      draft.homeCoinbaseWalletCard = false;
+      draft.homeCreateNewWalletStepper = true;
+      draft.homeImportWalletStepper = false;
+    });
+  },
+
+  SHOW_IMPORT_WALLET_STEPPER: (state, action) => {
+    return produce(state, (draft) => {
+      draft.homeCoinbaseWalletCard = false;
+      draft.homeCreateNewWalletStepper = true;
+      draft.homeImportWalletStepper = false;
+    });
+  },
+};
+
+const reducer = (state, action) => {
+  return handlers[action.type](state, action);
+};
+
 const Home = () => {
-  const [show, setShow] = useState({
-    homeCoinbaseWalletCard: false,
-    homeCreateNewWalletStepper: false,
-    homeImportWalletStepper: true,
-  });
+  const [state, dispatch] = useReducer(reducer, initialStates);
 
   const handleClickCreateNewWallet = () => {
-    setShow({
-      homeCoinbaseWalletCard: false,
-      homeCreateNewWalletStepper: true,
-      homeImportWalletStepper: false,
-    });
+    dispatch({ type: "SHOW_CREATE_NEW_WALLET_STEPPER" });
   };
 
   const handleClickImportExistingWallet = () => {
-    setShow({
-      homeCoinbaseWalletCard: false,
-      homeCreateNewWalletStepper: false,
-      homeImportWalletStepper: true,
-    });
+    dispatch({ type: "SHOW_IMPORT_WALLET_STEPPER" });
   };
 
   return (
     <>
-      {show.homeCoinbaseWalletCard && (
+      {state.homeCoinbaseWalletCard && (
         <Container maxWidth="xs" sx={{ mt: "10vh", px: 0 }}>
           <HomeCoinbaseWalletCard
             onClickCreateNewWallet={handleClickCreateNewWallet}
@@ -41,12 +59,12 @@ const Home = () => {
           />
         </Container>
       )}
-      {show.homeCreateNewWalletStepper && (
+      {state.homeCreateNewWalletStepper && (
         <Container maxWidth="md" sx={{ mt: "10vh", px: 0 }}>
           <HomeCreateNewWalletStepper />
         </Container>
       )}
-      {show.homeImportWalletStepper && (
+      {state.homeImportWalletStepper && (
         <Container maxWidth="md" sx={{ mt: "10vh", px: 0 }}>
           <HomeImportWalletStepper />
         </Container>
